89637fac99fdcb13a2c7982b151f1b10db09090f
IngridChou <113267284+IngridChou@users.noreply.github.com>
2023-09-17 19:52:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,32 +10,32 @@ function App() {
       <Title />
       <News>
         <NewsItem
-          newsImage="https://i.cbc.ca/1.6945190.1694824581!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/shutterstock-medium-file.jpg"
-          newsTitle="Artificial intelligence is being used in university classes. How it's being used matters, say profs"
-          newsLink="https://www.cbc.ca/news/canada/nova-scotia/artificial-intelligence-university-classes-research-1.6968138"
-          newsAuthor="Cassidy Chisholm"
-          newsDescription="2 professors weigh in on how large-language models may help — or hinder — student research"
+          newsImage="https://cleantechnica.com/files/2021/10/Tesla-China-Sales-e1642076454693.jpeg"
+          newsTitle="Tesla Shanghai Capacity Will Exceed 1 Million Cars In 2022, BYD Blade Batteries Coming"
+          newsLink="https://cleantechnica.com/2022/02/14/tesla-shanghai-capacity-will-exceed-1-million-cars-in-2022-byd-blade-batteries-coming/"
+          newsAuthor="Steve Hanley"
+          newsDescription="Tesla's Giga Shanghai is expanding and will soon be able to manufacture more than 1 million cars a year."
         />
         <NewsItem
-          newsImage="https://i.cbc.ca/1.6816594.1682000589!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/space-exploration-starship.jpg"
-          newsTitle="Elon Musk's SpaceX can't launch its giant rocket again until 63 fixes are made, FAA says"
-          newsLink="https://www.cbc.ca/news/science/spacex-faa-1.6956818"
-          newsAuthor="The Associated Press"
-          newsDescription="Closing its investigation into April explosion, U.S. agency says corrective actions needed before any relaunch"
+          newsImage="http://www.milenio.com/uploads/media/2022/02/13/elon-musk-estimo-deuda-unidos.jpeg"
+          newsTitle="Elon Musk asegura que la 'verdadera' deuda de EU es de al menos 60 billones de dólares"
+          newsLink="https://www.milenio.com/negocios/elon-musk-deuda-eu-60-billones-dolares"
+          newsAuthor="Milenio Digital"
+          newsDescription="El director ejecutivo de Tesla y SpaceX, Elon Musk, estimó que la deuda de Estados Unidos en tres veces mayor a su economía. ?La verdadera deuda nacional, incluidos los derechos no financiados, es de al menos 60 billones de dólares, aproximadamente tres veces el tamaño de toda la economía de ..."
         />
         <NewsItem
-          newsImage="https://i.cbc.ca/1.6595252.1664117761!/fileImage/httpImage/image.jpg_gen/derivatives/16x9_780/power-poles-snapped-glace-bay-fiona.jpg"
-          newsTitle="Climate change is affecting telecommunications infrastructure. Ham radio might be able to help"
-          newsLink="https://www.cbc.ca/news/canada/nova-scotia/cimate-change-telecommunications-ham-radio-help-1.6884108"
-          newsAuthor="Moira Donovan"
-          newsDescription="The technology is ‘an unsung hero’ for getting messages out, one operator says"
+          newsImage="https://www.teslarati.com/wp-content/uploads/2020/11/tesla-model-3-heat-pump-refresh.jpeg"
+          newsTitle="Tesla recall for heater and defroster systems issued by Transport Canada"
+          newsLink="https://www.teslarati.com/tesla-recall-heater-defroster-system-transport-canada/"
+          newsAuthor="Maria Merano"
+          newsDescription="Transport Canada issued a recall for the Tesla Model 3, Model Y, and Model S on February 3, 2022, the agency told Teslarati recently. The recall was issued after the agency discovered an issue with the heat system in certain Tesla vehicles.  Transport Canada reported that a little over ..."
         />
         <NewsItem
           newsImage="https://i.cbc.ca/1.6960149.1694135210!/cpImage/httpImage/image.jpg_gen/derivatives/16x9_780/ai-classroom-20230305.jpg"
-          newsTitle="The 'perfect' teaching assistant? Universities find new uses for AI"
-          newsLink="https://www.cbc.ca/news/ai-chatgpt-openai-universities-1.6958321"
-          newsAuthor="Joseph Pugh"
-          newsDescription="New wave of tools embraced by educators — and not just in computer labs"
+          newsTitle="China representó la mitad de las ventas globales de coches eléctricos en 2021"
+          newsLink="https://www.infobae.com/america/agencias/2022/02/14/china-represento-la-mitad-de-las-ventas-globales-de-coches-electricos-en-2021-2/"
+          newsAuthor="Newsroom Infobae"
+          newsDescription="Shanghái (China), 14 feb La mitad de las ventas de vehículos eléctricos registradas en 2021 a nivel mundial se efectuaron en China, según datos publicados hoy por la consultora Canalys. En total, según las estimaciones de Canalys, se vendieron 6,5 millones de este tipo de vehículos -incluyendo los totalmente ..."
         />
       </News>
     </div>
